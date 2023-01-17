83f96e14c598a2a846a2a12eca441715243c964a
abigs <abigs@LAPTOP-SDJ7VRRE>
2023-01-16 20:35:52 -0700

    Add project files.

---
+++ IS413/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("IS413")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("IS413")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bdc06d64-90fc-4eae-a2e5-71494370df8a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ IS414/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+class DiceGame
+{
+    static void Main()
+    {
+
+        int[] results = new int[13]; // An array that will keep track of the total for each roll
+        int rollCount; // number of times the dice are rolled
+        Random rng = new Random(); 
+
+        Console.WriteLine("Welcome to the dice throwing simulator! \n \n " );
+        Console.Write("How many dice rolls would you like to simulate?: ");
+        rollCount = int.Parse(Console.ReadLine());
+
+        // Dice rolls
+        for (int i = 0; i < rollCount; i++)
+        {
+            int dice1 = rng.Next(1, 7); // Dice 1
+            int dice2 = rng.Next(1, 7); // Dice 2
+            int total = dice1 + dice2; // Total of the two dices
+            results[total]++; // add the results to the array and increment 
+        }
+
+        Console.WriteLine("\n\nDICE ROLLING SIMULATION RESULTS  \nEach '*' represents 1 % of the total number of rolls. \nTotal number of rolls = " + rollCount + ".");
+
+
+        // Print the histogram with the * marks
+        for (int i = 2; i <= 12; i++)
+        {
+            double percentage = (double)results[i] / rollCount * 100; 
+            Console.Write(i + ": ");
+            for (int j = 0; j < percentage; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\n\nThank you for using the dice throwing simulator. Goodbye!");
+
+
+#if DEBUG // How to exit the console once the game is over
+        try
+        {
+            //...
+        }
+        finally
+        {
+            Console.WriteLine("\n\n\nPress enter to close...");
+            Console.ReadLine();
+        }
+        #endif
+    }
+}
